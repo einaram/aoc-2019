687c61e45c1a17a68df7cc8c34e391562e2be61a
einar <amarmite@gmail.com>
2019-12-07 13:35:37 +0100

    2-2, efter att ha läst instruksen

--- src/day2.py
+++ src/day2.py
@@ -1,6 +1,5 @@
 def opcode_runned(data_runner):
-    data_op = data_runner[:]
-    print(data_op[0:4])
+    data_op = data_runner.copy()
     for i in range(0,len(data_op),4):
         opcode = data_op[i]
         if opcode == 1:
@@ -20,7 +19,6 @@ def opcode_runned(data_runner):
 with open(r"resources\day2.csv") as infile:
     data = infile.readline().split(",")
 data = [int(x) for x in data]
-print(sum(data))
 
 assert opcode_runned([1,0,0,0,99]) == [2,0,0,0,99]
 assert opcode_runned([2,3,0,3,99]) == [2,3,0,6,99]
@@ -28,7 +26,6 @@ assert opcode_runned([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
 assert opcode_runned([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
 
 def prerunner(data,n=12, v = 2):
-
     data[1]=n
     data[2]=v
     return opcode_runned(data)[0]
@@ -41,8 +38,6 @@ def part2(data):
     for n in range(100):
         for v in range(100):
             if prerunner(data,n,v) == 19690720:
-                print(n,v)
+                print(f"2. {100 * n + v}")
                 break
-print(sum(data))
 part2(data)
-print(sum(data))
