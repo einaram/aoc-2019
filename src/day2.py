5523f7c7155dfc85e7f00516b8cafc656ff6b396
einar <amarmite@gmail.com>
2019-12-07 13:27:24 +0100

    day2-2 feil

--- src/day2.py
+++ src/day2.py
@@ -1,7 +1,8 @@
-def opcode_runner(data):
-    data = [int(x) for x in data]
-    for i in range(0,len(data),4):
-        opcode = data[i]
+def opcode_runned(data_runner):
+    data_op = data_runner[:]
+    print(data_op[0:4])
+    for i in range(0,len(data_op),4):
+        opcode = data_op[i]
         if opcode == 1:
             opfunc = lambda x,y: x+y
         elif opcode == 2:
@@ -9,24 +10,39 @@ def opcode_runner(data):
         elif opcode == 99:
             break
 
-        i_num1 = data[i+1]
-        i_num2 = data[i+2]
-        i_output = data[i+3]
-        data[i_output] = opfunc(data[i_num1],data[i_num2])
-    return data
+        i_num1 = data_op[i+1]
+        i_num2 = data_op[i+2]
+        i_output = data_op[i+3]
+        data_op[i_output] = opfunc(data_op[i_num1],data_op[i_num2])
+    return data_op
 
 
 with open(r"resources\day2.csv") as infile:
     data = infile.readline().split(",")
+data = [int(x) for x in data]
+print(sum(data))
 
-assert opcode_runner([1,0,0,0,99]) == [2,0,0,0,99]
-assert opcode_runner([2,3,0,3,99]) == [2,3,0,6,99]
-assert opcode_runner([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
-assert opcode_runner([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
+assert opcode_runned([1,0,0,0,99]) == [2,0,0,0,99]
+assert opcode_runned([2,3,0,3,99]) == [2,3,0,6,99]
+assert opcode_runned([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
+assert opcode_runned([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
 
+def prerunner(data,n=12, v = 2):
 
-data[1]=12
-data[2]=2
-data =opcode_runner(data)
-print(data)
-print(data[0])
+    data[1]=n
+    data[2]=v
+    return opcode_runned(data)[0]
+# part 1
+assert prerunner(data) == 9581917
+print("1 done", data[0])
+
+#part2
+def part2(data):   
+    for n in range(100):
+        for v in range(100):
+            if prerunner(data,n,v) == 19690720:
+                print(n,v)
+                break
+print(sum(data))
+part2(data)
+print(sum(data))
